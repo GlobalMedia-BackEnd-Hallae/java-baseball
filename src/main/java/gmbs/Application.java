9bbd5c4ef87b6fbd1e8926990e59ff58b285f5b5
JunSeop Kim <wnqtjq8958@naver.com>
2023-01-07 05:46:28 +0900

    feat : 객채 생성 후 게임 재시작 여부 확인

--- src/main/java/gmbs/Application.java
+++ src/main/java/gmbs/Application.java
@@ -1,8 +1,24 @@
 package gmbs;
 
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO : 기능 구현
+        RandomNumber randomNum = new RandomNumber();
+        Input input = new Input();
+        GameJudgement judge = new GameJudgement();
+        Playagain playagain = new Playagain();
+        boolean again = true;
+
+        while (again){
+            List<Integer> computer = randomNum.create();
+            String result = "";
+            while (!result.equals("3스트라이크")){
+                result = judge.judgement(computer, input.playerNumber());
+                System.out.println(result);
+            }
+            again = playagain.playagain();
+        }
     }
 }
