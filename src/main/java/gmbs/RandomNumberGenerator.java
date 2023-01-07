69a34e1487e7ae90fa7b03bc2ec4508439554a9c
Sy P <yongsa0221@gmail.com>
2023-01-08 03:37:58 +0900

    feat : 난수 생성 클래스 추가
    RandomNumberGenerator 클래스를 통해 난수 생성 기능 분리

---
+++ src/main/java/gmbs/RandomNumberGenerator.java
@@ -0,0 +1,25 @@
+package gmbs;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class RandomNumberGenerator {
+    private final Random random = new Random();
+
+    public int getRandomDigit() {
+        return random.nextInt(8) + 1;
+    }
+
+    public ArrayList<Integer> getRandomNumbers(int length) {
+        ArrayList<Integer> tempNumbers = new ArrayList<>();
+        int tempRandomDigit;
+        for (int i = 0; i < length; i++) {
+            do {
+                tempRandomDigit = getRandomDigit();
+            } while (tempNumbers.contains(tempRandomDigit));
+            tempNumbers.add(tempRandomDigit);
+        }
+        System.out.println(tempNumbers);
+        return tempNumbers;
+    }
+}
