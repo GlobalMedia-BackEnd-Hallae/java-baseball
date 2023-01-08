971346b986316fea39e61a351b5da8a630e7b0f5
chanwoo <chocw0402@gmail.com>
2023-01-08 17:44:44 +0900

    add : 최대 야구공 개수 상수화

--- src/main/java/gmbs/common/Constant.java
+++ src/main/java/gmbs/common/Constant.java
@@ -3,4 +3,5 @@ package gmbs.common;
 public class Constant {
     public static final String RESTART = "1";
     public static final String FINISH = "2";
+    public static final int MAX_BALL_COUNT = 3;
 }
