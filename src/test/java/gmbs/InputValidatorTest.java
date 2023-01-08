26ca2224afd60181fa0c7c8850b962c0b45b5de3
Sy P <yongsa0221@gmail.com>
2023-01-09 04:08:30 +0900

    test : isValidNumberInput 테스트 케이스 추가

--- src/test/java/gmbs/InputValidatorTest.java
+++ src/test/java/gmbs/InputValidatorTest.java
@@ -27,7 +27,7 @@ class InputValidatorTest {
 
     @ParameterizedTest
     @DisplayName("주어진 길이에 맞는 각 자리수가 서로 다른 자연수인지 확인한다")
-    @CsvSource(value = {"111, 3, false", "1, 1, true", "123, 4, false", "123, 3, true", "0, 1, false"})
+    @CsvSource(value = {"111, 3, false", "1, 1, true", "123, 4, false", "123, 3, true", "0, 1, false", "120, 3, false"})
     void isValidNumberInput(String input, int length, boolean expect) {
         boolean actual = validator.isValidNumberInput(input, length);
         assertThat(actual).isEqualTo(expect);
