4c66a96560822891f7839de578d6aa262469a175
JunSeop Kim <wnqtjq8958@naver.com>
2023-01-08 15:17:00 +0900

    refactor : 메서드 네이밍 수정

--- src/main/java/gmbs/GameJudgement.java
+++ src/main/java/gmbs/GameJudgement.java
@@ -4,16 +4,18 @@ import java.util.List;
 
 public class GameJudgement {
     Compare compare = new Compare();
-    public String judgement(List<Integer> computer, List<Integer> player){
-        int total = compare.Count(computer, player);
+    public String GameJudgement(List<Integer> computer, List<Integer> player) {
+        int IndexCheck = compare.Count(computer, player);
         int strike = compare.countStrike(computer, player);
-        int ball = total - strike;
+        int ball = IndexCheck - strike;
 
-        if(total == 0){
+        if (IndexCheck == 0) {
             return "낫싱";
-        }else if(strike == 0){
+        }
+        if (strike == 0) {
             return ball + "볼";
-        }else if(ball == 0){
+        }
+        if (ball == 0) {
             return strike + "스트라이크";
         }
         return ball + "볼 " + strike + "스트라이크";
--- src/main/java/gmbs/Input.java
+++ src/main/java/gmbs/Input.java
@@ -5,15 +5,22 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Input {
-    public List<Integer> playerNumber(){
-        System.out.println("숫자를 입력해주세요 : ");
-        Scanner scanner = new Scanner(System.in);
-        List<Integer> playerNum = new ArrayList<>();
-        String input = scanner.next();
 
-        for(String number: input.split("")){
-            playerNum.add(Integer.parseInt(number));
+    public List<Integer> playerNumber() {
+        try {
+            System.out.println("숫자를 입력해주세요 : ");
+            Scanner scanner = new Scanner(System.in);
+            List<Integer> playerNumber = new ArrayList<>();
+            String input = scanner.next();
+
+            for (String number : input.split("")) {
+                playerNumber.add(Integer.parseInt(number));
+            }
+            return playerNumber;
+        }catch (IllegalStateException e){
+            System.out.println("잘못된 수를 입력했습니다");
+            return playerNumber();
         }
-        return playerNum;
+
     }
 }
