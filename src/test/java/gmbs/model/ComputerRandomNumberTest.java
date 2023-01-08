44af7f42068d5c2520e80e82bc8e845fd64cbfbd
chanwoo <chocw0402@gmail.com>
2023-01-08 17:03:32 +0900

    test : 컴퓨터 랜덤 수 생성 테스트 중복 없는 세자리 랜덤 숫자 생성 테스트

---
+++ src/test/java/gmbs/model/ComputerRandomNumberTest.java
@@ -0,0 +1,28 @@
+package gmbs.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ComputerRandomNumberTest {
+
+    @DisplayName("중복 없는 세자리 랜덤 숫자를 생성한다")
+    @Test
+    void generate() {
+        ComputerRandomNumber computerRandomNumber = new ComputerRandomNumber();
+        List<Integer> generate = computerRandomNumber.generate();
+
+        List<Integer> actual = generate.stream()
+                .distinct()
+                .collect(Collectors.toList());
+
+        assertThat(actual.size()).isEqualTo(3);
+        for (Integer random : actual) {
+            assertThat(random).isBetween(1, 9);
+        }
+    }
+}
