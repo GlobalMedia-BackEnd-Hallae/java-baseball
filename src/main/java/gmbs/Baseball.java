51a4b028caa84844367fd14e4e8705d1a693540f
Sy P <yongsa0221@gmail.com>
2023-01-09 01:35:35 +0900

    test : baseball 테스트 클래스 추가

--- src/main/java/gmbs/Baseball.java
+++ src/main/java/gmbs/Baseball.java
@@ -13,7 +13,7 @@ public class Baseball {
 
     public int strikeCount(List<Integer> randomNumbers, List<Integer> userInput) {
         int strikeCount = 0;
-        for (int i = 0; i < Constant.inputLength; i++) {
+        for (int i = 0; i < userInput.size(); i++) {
             if (userInput.get(i).equals(randomNumbers.get(i))) {
                 strikeCount++;
             }
@@ -23,7 +23,7 @@ public class Baseball {
 
     public int ballCount(List<Integer> randomNumbers, List<Integer> userInput) {
         int ballCount = 0;
-        for (int i = 0; i < Constant.inputLength; i++) {
+        for (int i = 0; i < userInput.size(); i++) {
             if (randomNumbers.contains(userInput.get(i)) && !userInput.get(i).equals(randomNumbers.get(i))) {
                 ballCount++;
             }
@@ -31,7 +31,7 @@ public class Baseball {
         return ballCount;
     }
 
-    public boolean isStrikeOut(int strikeCount) {
+    private boolean isStrikeOut(int strikeCount) {
         return strikeCount == Constant.inputLength;
     }
 
