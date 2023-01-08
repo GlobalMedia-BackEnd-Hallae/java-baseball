d0da3528cf3b3009519f7100e275dc4ec7b62307
Chan531 <chan657312@gmail.com>
2023-01-08 14:18:13 +0900

    상수 모음

--- src/main/java/gmbs/Constant.java
+++ src/main/java/gmbs/Constant.java
@@ -1,7 +1,10 @@
 package gmbs;
 
 public class Constant {
-    public static final String EXPRESSION_DELIMETER = "";
+    private Constant() {
+        throw new IllegalStateException("Utility class");
+    }
+
     public static final int NUMBER_LENGTH = 3;
     public static final int NUMBER_RANGE = 9;
     public static final int ASCII_CODE_OF_ZERO = '0';
