82f08b0178fbec59dab6a7cbc789e0bb0e6c0dd8
JunSeop Kim <wnqtjq8958@naver.com>
2023-01-08 15:14:38 +0900

    refactor : 메서드 네이밍 수정

--- src/main/java/gmbs/Application.java
+++ src/main/java/gmbs/Application.java
@@ -5,20 +5,20 @@ import java.util.List;
 public class Application {
 
     public static void main(String[] args) {
-        RandomNumber randomNum = new RandomNumber();
+        RandomNumber randomNumber = new RandomNumber();
         Input input = new Input();
         GameJudgement judge = new GameJudgement();
-        Playagain playagain = new Playagain();
+        PlayAgain playagain = new PlayAgain();
         boolean again = true;
 
-        while (again){
-            List<Integer> computer = randomNum.create();
+        while (again) {
+            List<Integer> computer = randomNumber.create();
             String result = "";
-            while (!result.equals("3스트라이크")){
-                result = judge.judgement(computer, input.playerNumber());
+            while (!result.equals("3스트라이크")) {
+                result = judge.GameJudgement(computer, input.playerNumber());
                 System.out.println(result);
             }
-            again = playagain.playagain();
+            again = playagain.isPlayAgain();
         }
     }
 }
