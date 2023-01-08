87dfcde7aa0b8f0ad884a7a7d821b29a399f4153
Chan531 <chan657312@gmail.com>
2023-01-08 14:01:38 +0900

    수를 입력받음

---
+++ src/main/java/gmbs/Input.java
@@ -0,0 +1,22 @@
+package gmbs;
+
+import java.util.Scanner;
+
+public class Input {
+    // 수를 입력받는 함수
+    public String inputNumber() {
+        String number;
+
+        try {
+            System.out.print("숫자를 입력해주세요 : ");
+            Scanner scanner = new Scanner(System.in);
+            number = scanner.nextLine();
+            Checker checker = new Checker();
+            checker.checkNumber(number);
+            return number;
+        } catch (IllegalStateException error) {
+            System.out.println("잘못된 수를 입력하셨습니다.");
+            return inputNumber();
+        }
+    }
+}
