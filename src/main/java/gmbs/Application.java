43c41ea41d244f281004ccaa582caf18bea9c350
chanwoo <chocw0402@gmail.com>
2023-01-08 16:16:57 +0900

    refactor : Main Application 실행

--- src/main/java/gmbs/Application.java
+++ src/main/java/gmbs/Application.java
@@ -1,11 +1,17 @@
 package gmbs;
 
-import gmbs.game.NumberBaseballGame;
+import gmbs.domain.BaseballGameController;
+import gmbs.view.input.InputConsole;
+import gmbs.view.input.InputConsoleImpl;
+
+import java.util.Scanner;
 
 public class Application {
 
     public static void main(String[] args) {
-        NumberBaseballGame game = new NumberBaseballGame();
-        game.play();
+        Scanner scanner = new Scanner(System.in);
+        InputConsole inputConsole = new InputConsoleImpl(scanner);
+        BaseballGameController gameController = new BaseballGameController(inputConsole);
+        gameController.run();
     }
 }
