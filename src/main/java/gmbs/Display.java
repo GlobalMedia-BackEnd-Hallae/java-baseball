04bdf0bd309da47cfc7e82a0e2f72ff400f53128
Sy P <yongsa0221@gmail.com>
2023-01-08 23:00:56 +0900

    refactor : display 클래스 메서드명 변경

--- src/main/java/gmbs/Display.java
+++ src/main/java/gmbs/Display.java
@@ -1,23 +1,22 @@
 package gmbs;
 
 public class Display {
-    public static void playAgainDisplay(String yes, String no) {
+    public static void showPlayAgain(String yes, String no) {
         System.out.println("다시하려면 '" + yes + "'    종료하려면 '" + no + "'   입력");
     }
 
-    public static void numberInputDisplay(int length) {
+    public static void showNumberInputDisplay(int length) {
         System.out.println(length + "자리 정수 입력");
     }
 
-    public static void wrongInputDisplay() {
+    public static void showWrongInputDisplay() {
         System.out.println("잘못된 입력");
     }
 
-    public static void countsDisplay(int strikes, int balls) {
-        if(strikes == 0 && balls == 0) {
+    public static void showStrikeBallCounts(int strikes, int balls) {
+        if (strikes == 0 && balls == 0) {
             System.out.println("낫싱");
-        }
-        else {
+        } else {
             System.out.println(strikes + " 스트라이크    " + balls + " 볼");
         }
     }
