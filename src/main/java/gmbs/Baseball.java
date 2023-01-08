2496c382d41bb092a4d97660220b22638c87a416
Sy P <yongsa0221@gmail.com>
2023-01-08 23:04:20 +0900

    refactor : baseball 클래스 strike ball 인스턴스변수 삭제
    strike ball 인스턴스 변수로 접근하는 방식에서 메서드를 통해 반환 방식으로 변경

--- src/main/java/gmbs/Baseball.java
+++ src/main/java/gmbs/Baseball.java
@@ -1,39 +1,13 @@
 package gmbs;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Baseball {
     private final ArrayList<Integer> inputNumbers = new ArrayList<>();
     private final ArrayList<Integer> randomNumbers = new ArrayList<>();
     private final UserInput userInput = new UserInput();
     private final RandomNumberGenerator random = new RandomNumberGenerator();
-    private int strikes;
-    private int balls;
-
-    public int getStrikes() {
-        return strikes;
-    }
-
-    public int getBalls() {
-        return balls;
-    }
-
-    public void addStrikes() {
-        strikes++;
-    }
-
-    public void addBalls() {
-        balls++;
-    }
-
-    public void resetStrikes() {
-        strikes = 0;
-    }
-
-    public void resetBalls() {
-        balls = 0;
-    }
-
 
     public void setRandomNumbers() {
         randomNumbers.addAll(random.getRandomNumbers(3));
@@ -46,45 +20,54 @@ public class Baseball {
     }
 
     public void setInputNumbers() {
-        Display.numberInputDisplay(Constant.inputLength);
+        Display.showNumberInputDisplay(Constant.inputLength);
         if (!inputNumbers.isEmpty()) {
             inputNumbers.clear();
         }
         inputNumbers.addAll(userInput.getNumbers());
     }
 
-    private void check() {
+    public int strikeCount(List<Integer> randomNumbers, List<Integer> userInput) {
+        int strikeCount = 0;
         for (int i = 0; i < Constant.inputLength; i++) {
-            if (inputNumbers.get(i).equals(randomNumbers.get(i))) {
-                addStrikes();
-            } else if (randomNumbers.contains(inputNumbers.get(i))) {
-                addBalls();
+            if (userInput.get(i).equals(randomNumbers.get(i))) {
+                strikeCount++;
             }
         }
+        return strikeCount;
+    }
+
+    public int ballCount(List<Integer> randomNumbers, List<Integer> userInput) {
+        int ballCount = 0;
+        for (int i = 0; i < Constant.inputLength; i++) {
+            if (randomNumbers.contains(userInput.get(i)) && !userInput.get(i).equals(randomNumbers.get(i))) {
+                ballCount++;
+            }
+        }
+        return ballCount;
+    }
+
+    public boolean isStrikeOut(int strikeCount) {
+        return strikeCount == Constant.inputLength;
     }
 
     private void resetAll() {
         inputNumbers.clear();
         randomNumbers.clear();
-        resetStrikes();
-        resetBalls();
-    }
-
-    public void resetCounts() {
-        resetStrikes();
-        resetBalls();
     }
 
     public void playOneGame() {
+        int strikes;
+        int balls;
         resetAll();
         setRandomNumbers();
         showRandomNumbers();
-        while (strikes < Constant.MAX_STRIKES) {
-            resetCounts();
+        do {
             setInputNumbers();
-            check();
-            Display.countsDisplay(getStrikes(), getBalls());
-        }
+            strikes = strikeCount(randomNumbers, inputNumbers);
+            balls = ballCount(randomNumbers, inputNumbers);
+            Display.showStrikeBallCounts(strikes, balls);
+        } while (!isStrikeOut(strikes));
     }
 
     public void playBall() {
