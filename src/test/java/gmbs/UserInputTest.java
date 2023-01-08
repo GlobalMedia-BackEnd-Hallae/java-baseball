0722706394fb935c504d789fca04991e9d1d2b88
Sy P <yongsa0221@gmail.com>
2023-01-08 11:24:34 +0900

    test : userInput 검증 메서드 테스트 추가

--- src/test/java/gmbs/UserInputTest.java
+++ src/test/java/gmbs/UserInputTest.java
@@ -10,10 +10,26 @@ class UserInputTest {
     private final UserInput userInput = new UserInput();
 
     @ParameterizedTest
-    @DisplayName("입력값이 정수인지 확인한다")
-    @CsvSource(value = {"123, true", "123.4, false", "123f, false", "-123, true", "abc, false", "0, true"})
-    void isInt(String input, boolean expect) {
-        boolean actual = userInput.isInt(input);
+    @DisplayName("입력값이 자연수인지 확인한다")
+    @CsvSource(value = {"123, true", "123.4, false", "123f, false", "-123, false", "abc, false", "0, false"})
+    void isNaturalNumber(String input, boolean expect) {
+        boolean actual = userInput.isNaturalNumber(input);
+        assertThat(actual).isEqualTo(expect);
+    }
+
+    @ParameterizedTest
+    @DisplayName("입력값에 중복이 있는지 확인한다")
+    @CsvSource(value = {"1123, false", "asdf, true", "13a3, false", "31, true"})
+    void hasNoOverlap(String input, boolean expect) {
+        boolean actual = userInput.hasNoOverlap(input);
+        assertThat(actual).isEqualTo(expect);
+    }
+
+    @ParameterizedTest
+    @DisplayName("주어진 길이에 맞는 각 자리수가 서로 다른 자연수인지 확인한다")
+    @CsvSource(value = {"111, 3, false", "1, 1, true", "123, 4, false", "123, 3, true", "0, 1, false"})
+    void isValidNumberInput(String input, int length, boolean expect) {
+        boolean actual = userInput.isValidNumberInput(input, length);
         assertThat(actual).isEqualTo(expect);
     }
 }
