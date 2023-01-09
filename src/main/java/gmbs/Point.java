6924b19e966edada1ae553fb3cce019f027d0871
Chan531 <chan657312@gmail.com>
2023-01-10 01:52:58 +0900

    Result 클래스 활용을 위해 int 형에서 boolean으로 수정

--- src/main/java/gmbs/Point.java
+++ src/main/java/gmbs/Point.java
@@ -2,19 +2,11 @@ package gmbs;
 
 public class Point {
     // 볼인지 스트라이크인지 체크해주는 함수
-    public int checkBall(int result) {
-        if (result == Constant.BALL) {
-            return 1;
-        }
-
-        return 0;
+    public boolean checkBall(int result) {
+        return result == Constant.BALL;
     }
 
-    public int checkStrike(int result) {
-        if (result == Constant.STRIKE) {
-            return 1;
-        }
-
-        return 0;
+    public boolean checkStrike(int result) {
+        return result == Constant.STRIKE;
     }
 }
