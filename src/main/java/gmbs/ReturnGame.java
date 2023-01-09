32038b87d551bf4de30ce51f3bcd66a0b3755764
Chan531 <chan657312@gmail.com>
2023-01-10 01:56:14 +0900

    Again을 ReturnGame으로 변경

--- src/main/java/gmbs/ReturnGame.java
+++ src/main/java/gmbs/ReturnGame.java
@@ -2,10 +2,11 @@ package gmbs;
 
 import java.util.Scanner;
 
-public class Again {
+public class ReturnGame {
+    private Scanner scanner = new Scanner(System.in);
+
     public int returnGame() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        Scanner scanner = new Scanner(System.in);
         return scanner.nextInt();
     }
 }
