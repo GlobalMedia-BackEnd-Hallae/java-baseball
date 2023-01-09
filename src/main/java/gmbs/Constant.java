d3973ed5fe789c28b93af5b399dab8775cff412f
Chan531 <chan657312@gmail.com>
2023-01-10 01:50:20 +0900

    상수 부분 수정

--- src/main/java/gmbs/Constant.java
+++ src/main/java/gmbs/Constant.java
@@ -1,13 +1,9 @@
 package gmbs;
 
 public class Constant {
-    private Constant() {
-        throw new IllegalStateException("Utility class");
-    }
-
+    public static final int ASCII_CODE_OF_ZERO = '0';
     public static final int NUMBER_LENGTH = 3;
     public static final int NUMBER_RANGE = 9;
-    public static final int ASCII_CODE_OF_ZERO = '0';
     public static final int NOT_EXIST = -1;
     public static final int BALL = 0;
     public static final int STRIKE = 1;
