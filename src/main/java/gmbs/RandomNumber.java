2a0c8f8a60825905960e937718c48899b734cd88
JunSeop Kim <wnqtjq8958@naver.com>
2023-01-10 00:00:19 +0900

    refactor : 매직 넘버 치환

--- src/main/java/gmbs/RandomNumber.java
+++ src/main/java/gmbs/RandomNumber.java
@@ -6,17 +6,19 @@ import java.util.Random;
 
 public class RandomNumber {
 
-    private static final int MIN_NUMBER = 1;
+    private static final int MINIMUN_NUMBER = 1;
+    private static final int MAXIMUM_NUMBER = 9;
+    private static final int MAXIMUM_COMPUTER_NUMBER = 3;
 
     private int MakeRandomNumber() {
         Random random = new Random();
-        return random.nextInt(9) + MIN_NUMBER;
+        return random.nextInt(MAXIMUM_NUMBER) + MINIMUN_NUMBER;
     }
 
-    List<Integer> create() {
+    public List<Integer> create() {
         List<Integer> computerNumber = new ArrayList<>();
 
-        while (computerNumber.size() < 3) {
+        while (computerNumber.size() < MAXIMUM_COMPUTER_NUMBER) {
             int randomNumber = MakeRandomNumber();
             if (computerNumber.contains(randomNumber)) {
                 continue;
