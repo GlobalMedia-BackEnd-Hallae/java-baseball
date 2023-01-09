e3705dc4a051534386b01bb1b3f126c55ede50ba
JunSeop Kim <wnqtjq8958@naver.com>
2023-01-09 23:59:52 +0900

    refactor : 매직 넘버 치환

--- src/main/java/gmbs/PlayAgain.java
+++ src/main/java/gmbs/PlayAgain.java
@@ -4,14 +4,15 @@ import java.util.Scanner;
 
 public class PlayAgain {
 
+    private static final String GAME_RESTART_OR_GAME_END = "3개의 숫자를 모두 맞히셨습니다! 게임 종료 다시 시작 : 1 종료 : 2";
     private static final int GAME_RESTART = 1;
-    private static final int GAME_END = 2;
-    private int answer;
 
-    public boolean isPlayAgain() {
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료 다시 시작 : 1, 종료 : 2");
-        answer = new Scanner(System.in).nextInt();
-        if (this.answer == GAME_RESTART) {
+    private final Scanner scanner = new Scanner(System.in);
+
+    public boolean PlayAgain() {
+        System.out.println(GAME_RESTART_OR_GAME_END);
+        int answer = scanner.nextInt();
+        if (answer == GAME_RESTART) {
             return true;
         }
         return false;
