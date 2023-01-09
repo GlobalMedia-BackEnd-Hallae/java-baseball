a656049f122f170e95938b15aa0ed61e26be2a80
Chan531 <chan657312@gmail.com>
2023-01-10 01:54:48 +0900

    볼, 스트라이크, 낫싱 여부 세부적으로 테스트

--- src/test/java/gmbs/PointTest.java
+++ src/test/java/gmbs/PointTest.java
@@ -6,20 +6,30 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class PointTest {
-    Point point = new Point();
+    private Point point = new Point();
 
     @Test
-    @DisplayName("테스트 해보기")
+    @DisplayName("볼, 스트라이크, 낫싱 여부 테스트")
     void canPoint() {
         // given
-        int result = Constant.BALL;
+        int ball = Constant.BALL;
+        int strike = Constant.STRIKE;
+        int notExist = Constant.NOT_EXIST;
 
         // when
-        int ballResult = point.checkBall(result);
-        int strikeResult = point.checkStrike(result);
+        boolean ballResultOfCheckBall = point.checkBall(ball);
+        boolean ballResultOfCheckStrike = point.checkStrike(ball);
+        boolean strikeResultOfCheckBall = point.checkBall(strike);
+        boolean strikeResultOfCheckStrike = point.checkStrike(strike);
+        boolean nothingResultOfCheckBall = point.checkBall(notExist);
+        boolean nothingResultOfCheckStrike = point.checkStrike(notExist);
 
         // then
-        assertThat(ballResult).isEqualTo(1);
-        assertThat(strikeResult).isZero();
+        assertThat(ballResultOfCheckBall).isTrue();
+        assertThat(ballResultOfCheckStrike).isFalse();
+        assertThat(strikeResultOfCheckBall).isFalse();
+        assertThat(strikeResultOfCheckStrike).isTrue();
+        assertThat(nothingResultOfCheckBall).isFalse();
+        assertThat(nothingResultOfCheckStrike).isTrue();
     }
 }
