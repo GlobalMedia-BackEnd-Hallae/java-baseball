63ac8712bad17230f3c62ea56903e1a74a102bd3
JunSeop Kim <wnqtjq8958@naver.com>
2023-01-09 23:58:51 +0900

    refactor : 구분자 수정

--- src/main/java/gmbs/Input.java
+++ src/main/java/gmbs/Input.java
@@ -6,21 +6,21 @@ import java.util.Scanner;
 
 public class Input {
 
+    private final String separator = "";
+    private final Scanner scanner = new Scanner(System.in);
+
     public List<Integer> playerNumber() {
         try {
             System.out.println("숫자를 입력해주세요 : ");
-            Scanner scanner = new Scanner(System.in);
             List<Integer> playerNumber = new ArrayList<>();
             String input = scanner.next();
-
-            for (String number : input.split("")) {
+            for (String number : input.split(separator)) {
                 playerNumber.add(Integer.parseInt(number));
             }
             return playerNumber;
-        }catch (IllegalStateException e){
+        } catch (IllegalArgumentException e) {
             System.out.println("잘못된 수를 입력했습니다");
             return playerNumber();
         }
-
     }
 }
