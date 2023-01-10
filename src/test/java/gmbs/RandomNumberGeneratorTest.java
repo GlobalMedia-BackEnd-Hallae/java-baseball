185869aa84649246d9f351656fd1a77b4abfadde
Sy P <yongsa0221@gmail.com>
2023-01-10 12:33:48 +0900

    test : RandomNumberGeneratorTest given when then 추가

--- src/test/java/gmbs/RandomNumberGeneratorTest.java
+++ src/test/java/gmbs/RandomNumberGeneratorTest.java
@@ -18,19 +18,26 @@ class RandomNumberGeneratorTest {
     @DisplayName("입력된 길이만큼 중복없는 숫자들을 반환한다")
     @ValueSource(ints = {1, 2, 3, 4, 5})
     void getRandomNumbers(int length) {
-        HashSet<Integer> randomNumbers = new HashSet<>(random.getRandomNumbers(length));
-        assertThat(randomNumbers).hasSize(length);
+        //given
+        int testLength = length;
+        //when
+        HashSet<Integer> randomNumbers = new HashSet<>(random.getRandomNumbers(testLength));
+        //then
+        assertThat(randomNumbers).hasSize(testLength);
     }
 
     @Test
     @DisplayName("한자리 자연수를 반환한다")
     void getRandomDigit() {
+        //given
         int testCount = 1000;
         HashSet<Integer> randomNumbers = new HashSet<>();
         HashSet<Integer> expected = new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
+        //when
         for (int i = 0; i < testCount; i++) {
             randomNumbers.add(random.getRandomDigit());
         }
+        //then
         assertThat(randomNumbers).isEqualTo(expected);
     }
 }
